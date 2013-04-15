931b6006b05cbfb96b36c20f6109ed9d5ff8d32b
phxsuns <sunxuhao@gmail.com>
2013-04-15 22:31:24 +0800

    对浏览器兼容性进行了处理

--- static/js/global.js
+++ static/js/global.js
@@ -20,4 +20,11 @@ $(function(){
 		window.location.href = url;
 	});
 
+	//浏览器兼容性
+	//IE678，提示
+	if (!$.support.leadingWhitespace){
+		$('<div class="container" style="text-align:center;border:1px solid #FFD1BF;background-color: #FFEBE2;color: #9B0013;padding:5px;">您正在使用的是非标准浏览器。推荐使用Chrome和FireFox访问本站。</div>').prependTo($('body')).hide().slideDown();
+	}
+
+
 });
